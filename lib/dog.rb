bf0aa39ec47656606b4f8464e2e9c9c64cde5ae5
Rida Kodad <kodadreda@gmail.com>
2019-08-08 13:39:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,12 @@ class Dog
     @name = new_name
   end
 
-  def Breed
-  @Breed
+  def breed
+  @breed
   end
 
-  def Breed=(new_breed)
-    @Breed = new_breed
+  def breed=(new_breed)
+    @breed = new_breed
   end
 
 end
